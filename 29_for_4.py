7ecd1a7d07a4cb8ddd2818355bcfd02a9b1b9635
theeasylearn <theeasylearn@gmail.com>
2025-11-06 09:51:45 +0530

    next day preparation

--- 29_for_4.py
+++ 29_for_4.py
@@ -1,1 +1,2 @@
 # using for loop with string
+line = "Quick zebra jumps over lazy 3 dogs at Delhi park around 12:30 pm near cage no 5 @ visitor day"
---
+++ 30_for_5.py
@@ -0,0 +1,2 @@
+#for loop with dictionary
+players = {"Rohit":78, "Gill":45, "Kohli":92, "Iyer":36, "Rahul":58, "Hardik":24, "Jadeja":41, "Ashwin":15, "Shami":10, "Bumrah":5, "Siraj":8}
